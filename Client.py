d64b2001e9f0c5a77f965bd24566fe76df6df11b
gracewalkuski <35305141+gracewalkuski@users.noreply.github.com>
2018-09-24 14:48:09 -0600

    Update Client.py

--- Client.py
+++ Client.py
@@ -54,6 +54,10 @@ class Client(threading.Thread):
         lines[x] = ''.join(target_row)
         opp_board_r.close()
 
+        print('Opponent Board:')
+        for i in range (10):
+            print(lines[i])
+
         filepath = os.path.join('c:/Users/ian/Documents/Courses/CSCI 466/Program1', 'opponent_board.txt')
         opp_board_w = open(filepath, 'w')
         opp_board_w.write(''.join(lines))
@@ -141,23 +145,3 @@ class Client(threading.Thread):
                     self.my_turn = True
                     
                 time.sleep(1)
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                    
-                
